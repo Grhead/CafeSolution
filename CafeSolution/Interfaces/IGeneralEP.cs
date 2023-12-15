62544913d6cd7a890ace9fdd52af0ba123cdfb14
Grenka <grenogrenka@gmail.com>
2023-12-16 02:30:47 +0700

    remove unnecessary files

--- CafeSolution/Interfaces/IGeneralEP.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using CafeSolution.Models;
-
-namespace CafeSolution.Interfaces;
-
-public interface IGeneralEp
-{
-    Employee Auth(string login, string password);
-}
--- CafeSolution/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace CafeSolution.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
