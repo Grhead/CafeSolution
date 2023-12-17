e7875437ba8f9a8c6afce4aee911c969397f52b0
Grenka <grenogrenka@gmail.com>
2023-12-17 18:38:15 +0700

    List changes

--- CafeSolutionWPF/ViewModels/CookViewModel.cs
+++ CafeSolutionWPF/ViewModels/CookViewModel.cs
@@ -1,11 +1,9 @@
+using CafeSolutionWPF.Models;
+
 namespace CafeSolutionWPF.ViewModels;
 
 public class CookViewModel
 {
-    public object OrderId { get; }
-    public object OrderCookingStatus { get; }
-    public object DishTitle { get; }
-    public object StatusList { get; }
-    public object selectedStatus { get; }
     public object selectedOrder { get; }
+    public Order OrderView { get; }
 }
